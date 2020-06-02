563d709edcbaff2418e1e81d7beca8212dcf0f8f
Agata Toczyńska <agata.toczynska@capgemini.com>
2020-06-02 19:24:00 +0200

    Updates

---
+++ precache-manifest.4a8cf8637ee4854133b472769aeb68a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2aed144eb89665a72067ed2bc253258c",
+    "url": "/ikwita/index.html"
+  },
+  {
+    "revision": "cd348ca84fbe8ad11236",
+    "url": "/ikwita/static/css/2.9950865f.chunk.css"
+  },
+  {
+    "revision": "9c46b1a9066963342805",
+    "url": "/ikwita/static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "cd348ca84fbe8ad11236",
+    "url": "/ikwita/static/js/2.45865a2c.chunk.js"
+  },
+  {
+    "revision": "0d3abb31b625e1e39fa410c095c74c86",
+    "url": "/ikwita/static/js/2.45865a2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c46b1a9066963342805",
+    "url": "/ikwita/static/js/main.f9709133.chunk.js"
+  },
+  {
+    "revision": "96e4cb9d16e4b2984c4a",
+    "url": "/ikwita/static/js/runtime-main.1d11d7e3.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "ikwita/precache-manifest.362dfcb261da8c1af05d50c67310e702.js"
+  "/ikwita/precache-manifest.4a8cf8637ee4854133b472769aeb68a4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("ikwita/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ikwita/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
