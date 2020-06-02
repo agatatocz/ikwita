d8e7dcc9f2632d1034b6e9ce2b2c49a60029488b
Agata Toczyńska <agata.toczynska@capgemini.com>
2020-06-02 19:09:42 +0200

    Updates

---
+++ precache-manifest.d89a0114c405df34848f43ce1400e809.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdbcc5745acb52e2caf7ce583263dcd7",
+    "url": "https://agatatocz.github.io/ikwita/index.html"
+  },
+  {
+    "revision": "cd348ca84fbe8ad11236",
+    "url": "https://agatatocz.github.io/ikwita/static/css/2.9950865f.chunk.css"
+  },
+  {
+    "revision": "9c1eab7be0b88643868e",
+    "url": "https://agatatocz.github.io/ikwita/static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "cd348ca84fbe8ad11236",
+    "url": "https://agatatocz.github.io/ikwita/static/js/2.45865a2c.chunk.js"
+  },
+  {
+    "revision": "0d3abb31b625e1e39fa410c095c74c86",
+    "url": "https://agatatocz.github.io/ikwita/static/js/2.45865a2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c1eab7be0b88643868e",
+    "url": "https://agatatocz.github.io/ikwita/static/js/main.d21a5039.chunk.js"
+  },
+  {
+    "revision": "c67218a60f30b287b5b6",
+    "url": "https://agatatocz.github.io/ikwita/static/js/runtime-main.5e51b695.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ikwita/precache-manifest.78d11c261be13d89630b043b22d939a0.js"
+  "https://agatatocz.github.io/ikwita/precache-manifest.d89a0114c405df34848f43ce1400e809.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ikwita/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://agatatocz.github.io/ikwita/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
